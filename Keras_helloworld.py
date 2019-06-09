2a43a04e57ef6fbd3c6e8182c78b64ef60ce9020
Amitai Dabbah <46689328+dorimet0@users.noreply.github.com>
2019-06-09 21:13:44 +0300

    Create Keras_helloworld.py

---
+++ Keras_helloworld.py
@@ -0,0 +1,21 @@
+	#simple neural network implementation using keras front-end
+  
+  import tensorflow as tf
+	from tensorflow.keras.models import Sequential
+	from tensorflow.keras.layers import Dense
+
+
+	import numpy as np
+	x_input = np.array([[1,2,3,4,5]])
+	y_input = np.array([[10]])
+
+
+	model = Sequential()
+	model.add(Dense(units=32, activation="tanh", input_dim=x_input.shape[1], kernel_initializer='random_normal'))
+	model.add(Dense(units=1, kernel_initializer='random_normal'))
+
+
+	model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+
+
+	model.summary()
